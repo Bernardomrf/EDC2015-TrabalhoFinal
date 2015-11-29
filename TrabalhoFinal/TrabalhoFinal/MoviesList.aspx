818628aaca4ffe40fa9820802e1300571b374d73
Bernardomrf <bernardomrferreira@ua.pt>
2015-11-29 19:05:30 +0000

    Lista de filmes done

--- TrabalhoFinal/TrabalhoFinal/MoviesList.aspx
+++ TrabalhoFinal/TrabalhoFinal/MoviesList.aspx
@@ -1,6 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MoviesList.aspx.cs" Inherits="TrabalhoFinal.MoviesList" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
+    <h1><i class="fa fa-film"></i> Movies List</h1>
+    <hr />
 
-
+    <asp:XmlDataSource ID="XmlDataSource1" runat="server" TransformFile="~/App_Data/MoviesList.xsl" EnableCaching="false"></asp:XmlDataSource>
+    <div id="wrapper">
+        <div class ="center">
+            <div class ="row">
+                <div id="news" runat="server" class="list">
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- TrabalhoFinal/TrabalhoFinal/MoviesList.aspx.cs
+++ TrabalhoFinal/TrabalhoFinal/MoviesList.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace TrabalhoFinal
 {
@@ -12,6 +14,43 @@ namespace TrabalhoFinal
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            XmlReader reader = XmlReader.Create("http://www.myapifilms.com/imdb/top?format=XML&start=1&end=250&data=F");
+            XmlDocument doc = new XmlDocument();
+            doc.Load(reader);
+            reader.Close();
+
+
+            XmlDataSource1.Data = doc.OuterXml;
+            XmlDataSource1.DataBind();
+            XmlDataSource1.XPath = "/channel";
+
+            XmlDocument xdoc2 = XmlDataSource1.GetXmlDocument();
+            XmlNodeList noticias = xdoc2.SelectNodes("//movie");
+            news.InnerHtml = "<ul>";
+            
+
+            foreach (XmlNode i in noticias)
+            {
+                Debug.WriteLine(i.Attributes["title"].Value);
+
+                news.InnerHtml += ""+
+                
+                "<li>" +
+                    "<a href = \"/filme/"+ i.Attributes["idIMDB"].Value + "\" >" +
+                        "<div class=\"thumb\">" +
+                            "<div class=\"img\" style=\"background-image: url('"+i.Attributes["urlPoster"].Value + "');\"></div>"+
+                        "</div>" + 
+                        "<div class=\"info\">"+
+                            "<div class=\"title\">"+ i.Attributes["title"].Value + "</div>" + 
+                            "<div class=\"infos\">" +
+                                "<div class=\"year\">" + i.Attributes["year"].Value + "</div>" +
+                                "<div class=\"imdb\">TMDB: " + i.Attributes["rating"].Value + "</div>" +
+                            "</div>"+
+                        "</div>"+
+                    "</a>"+
+                "</li>";
+            }
+            news.InnerHtml += "</ul>";
         }
     }
 }
